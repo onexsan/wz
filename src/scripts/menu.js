c1ccb6f0a30faf5e0c486d9dc3cb6bd3092b3b91
Victoria Filileeva <onexsan@gmail.com>
2020-08-15 16:13:03 +0300

    the order menu added

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,6 +2,10 @@ const burger__menu = document.querySelector('.burger-menu__link');
 const main__menu = document.querySelector('.main-menu');
 const main__menuBtn = document.querySelector('.main-menuBtn');
 
+const order__call = document.querySelector('.call__link');
+const ordered = document.querySelector('.ordered');
+const orderedBtn = document.querySelector('.orderedBtn-closed');
+
 burger__menu.addEventListener('click', function (e) {
 	e.preventDefault();
 	main__menu.style["display"] = 'flex';
@@ -11,6 +15,17 @@ burger__menu.addEventListener('click', function (e) {
 main__menuBtn.addEventListener('click', function (e) {
 	e.preventDefault();
 	main__menu.style["display"] = 'none';
+});
+
+order__call.addEventListener('click', function (e) {
+	e.preventDefault();
+	ordered.style["display"] = 'flex';
+
+});
+
+orderedBtn.addEventListener('click', function (e) {
+	e.preventDefault();
+	ordered.style["display"] = 'none';
 })
 
 
