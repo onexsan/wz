8ff98f45202b5bd2f9a8897c910d14f0a1dfdad1
Victoria Filileeva <onexsan@gmail.com>
2020-08-16 15:12:33 +0300

    carousels added

--- gulp.config.js
+++ gulp.config.js
@@ -2,9 +2,12 @@ module.exports = {
   SRC_PATH: "src",
   DIST_PATH: "dist",
   STYLES_LIBS: [
-    'node_modules/normalize.css/normalize.css'
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/owl.carousel/dist/assets/owl.carousel.css',
+    'node_modules/owl.carousel/dist/assets/owl.theme.default.css',
   ],
   JS_LIBS: [
-    //'node_modules/jquery/dist/jquery.js'
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/owl.carousel/dist/owl.carousel.min.js'
   ]
 }
---
+++ src/scripts/owlDots.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+	const owl = $('.owlDots').owlCarousel({
+		items: 1,
+		center: true,
+		nav: false,
+		loop: true,
+		dots: true,
+		dotsClass: 'container-dots',
+		dotClass: 'circle-btn'
+
+	});
+});
